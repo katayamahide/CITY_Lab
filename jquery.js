6cd33935a5a279dcb85107ad4e4cc0c241152248
ohata <ohata@ohatanoMacBook-Pro.local>
2020-10-17 17:36:43 +0900

    humbargar menu

--- jquery.js
+++ jquery.js
@@ -1,6 +1,14 @@
 
   $(function() {
-    $(".menu-icon").click(function(){
-      $(".menu-icon").toggleClass("close");
+    $('.menu-icon').click(function(){
+      if($('.menu-icon').hasClass('close')){
+        $(this).addClass('fa-bars').removeClass('fa-times close');
+        $('.header-right').removeClass('header-right-active');
+        $('.header-nav').removeClass('header-nav-active')
+      } else {
+        $(this).removeClass('fa-bars').addClass('fa-times close');
+        $('.header-right').addClass('header-right-active');
+        $('.header-nav').addClass('header-nav-active')
+      }
     })
   });
